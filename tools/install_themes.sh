b794937fa56559a0cf99316c82c06417aaa608e9
Asta <vine9151@gmail.com>
2023-03-21 15:40:20 +0900

    fix: fix order of commands in 'install_themes.sh'
    230321_15_52

--- tools/install_themes.sh
+++ tools/install_themes.sh
@@ -62,16 +62,16 @@ sudo make install
 
 
 ##======================
+#-- Openbox
+cp -rf $HOME/Documents/openbox-dotfiles/.themes $HOME/ || cp -rf ~/.config/.themes $HOME/
+
+
+##======================
 #-- GTK
 wget https://github.com/catppuccin/gtk/releases/download/v0.4.1/Catppuccin-Frappe-Standard-Maroon-Dark.zip
 unzip Catppuccin-Frappe-Standard-Maroon-Dark.zip -d $HOME/Documents/catppuccin-gtk/
 cp -rf $HOME/Documents/catppuccin-gtk/Catppuccin-Frappe-Standard-Maroon-Dark $HOME/.themes/
 rm Catppuccin-Frappe-Standard-Maroon-Dark.zip
-
-
-##======================
-#-- Openbox
-cp -rf $HOME/Documents/openbox-dotfiles/.themes $HOME/ || cp -rf ~/.config/.themes $HOME/
 
 
 ##======================
