39b98aefe46fd7afb1fc49da517403a92b283714
Aru <vine9151@gmail.com>
2023-05-05 23:11:06 +0900

    fix: bugfix in script for tint2 battery icons
    230505_23_11

--- tint2/scripts/battery-icon.sh
+++ tint2/scripts/battery-icon.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script displays battery icon according to the charge level and charging state
 
@@ -9,30 +9,49 @@
 # License: GPL3
 
 # Dependencies: `acpi`
+# Edited: Aru
+
 
 bat=$(acpi -b)
-state=$(echo ${bat} | awk '{print $3}')
+state0=$(echo ${bat} | awk '{print $3}')
+state1=$(echo ${bat} | awk '{print $10}')
 svg_dir=$HOME/.config/tint2/images
 
-if [[ "$state" = "Not" ]]; then
-  level=$(echo ${bat} | awk '{print $5}')
-  level=${level::-1}
-
-else
-  level=$(echo ${bat} | awk '{print $4}')
-  if [[ "$state" == *"Unknown"* ]]; then
+if [[ "$state0" != "Unknown," ]]; then
+  if [[ "$state0" = "Not" ]]; then
+    level=$(echo ${bat} | awk '{print $5}')
     level=${level::-1}
   else
-    if [[ "$level" == "100%" ]]; then
+    level=$(echo ${bat} | awk '{print $4}')
+    if [[ "$state0" == *"Unknown"* ]]; then
       level=${level::-1}
     else
-      level=${level::-2}
+      if [[ "$level" == "100%" ]]; then
+        level=${level::-1}
+      else
+        level=${level::-2}
+      fi
+    fi
+  fi
+elif [[ "$state1" != "Unknown," ]]; then
+  if [[ "$state1" = "Not" ]]; then
+    level=$(echo ${bat} | awk '{print $12}')
+    level=${level::-1}
+  else
+    level=$(echo ${bat} | awk '{print $11}')
+    if [[ "$state1" == *"Unknown"* ]]; then
+      level=${level::-1}
+    else
+      if [[ "$level" == "100%" ]]; then
+        level=${level::-1}
+      else
+        level=${level::-2}
+      fi
     fi
   fi
 fi
 
 if [[ "$bat" == *"until"* ]]; then
-
   if [[ "$level" -ge "95" ]]; then
     echo ${svg_dir}/bat-full-charging.svg
   elif [[ "$level" -ge "75" ]]; then
@@ -57,6 +76,7 @@ else
     echo ${svg_dir}/bat-empty.svg
   fi
 fi
+
 if  [[ $1 = "-l" ]]; then
     echo ${level}%
 fi
